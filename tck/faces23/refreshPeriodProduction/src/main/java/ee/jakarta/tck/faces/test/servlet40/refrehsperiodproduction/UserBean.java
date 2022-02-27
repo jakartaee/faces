9d2506c94ebc32642055194bfce9ec58622201e1
Bauke Scholtz <balusc@gmail.com>
2022-02-27 13:42:50 -0400

    Migrate test/servlet40/refreshPeriod* to tck/faces23

---
+++ tck/faces23/refreshPeriodProduction/src/main/java/ee/jakarta/tck/faces/test/servlet40/refrehsperiodproduction/UserBean.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2017, 2018 Oracle and/or its affiliates. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0, which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the
+ * Eclipse Public License v. 2.0 are satisfied: GNU General Public License,
+ * version 2 with the GNU Classpath Exception, which is available at
+ * https://www.gnu.org/software/classpath/license.html.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ */
+
+package ee.jakarta.tck.faces.test.servlet40.refrehsperiodproduction;
+
+import java.io.Serializable;
+
+import com.sun.faces.application.ApplicationAssociate;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.faces.context.ExternalContext;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+
+@Named
+@RequestScoped
+public class UserBean implements Serializable {
+    
+    protected String firstName;
+    @Inject 
+    ExternalContext extContext;
+    
+    public UserBean() {
+    }
+    
+    @PostConstruct
+    private void init() {
+        ApplicationAssociate appAss = ApplicationAssociate.getInstance(extContext);
+        firstName = "" + appAss.getFaceletFactory().getRefreshPeriod();
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+}
+
