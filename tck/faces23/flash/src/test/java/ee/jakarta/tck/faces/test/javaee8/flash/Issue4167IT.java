38d6357d65800077160a345f5c437bd73e64ba91
Bauke Scholtz <balusc@gmail.com>
2022-02-27 09:49:12 -0400

    Migrate faces23/flash test to TCK

---
+++ tck/faces23/flash/src/test/java/ee/jakarta/tck/faces/test/javaee8/flash/Issue4167IT.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 1997, 2018 Oracle and/or its affiliates. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0, which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the
+ * Eclipse Public License v. 2.0 are satisfied: GNU General Public License,
+ * version 2 with the GNU Classpath Exception, which is available at
+ * https://www.gnu.org/software/classpath/license.html.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ */
+
+package ee.jakarta.tck.faces.test.javaee8.flash;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.html.HtmlSubmitInput;
+
+import ee.jakarta.tck.faces.test.util.arquillian.ITBase;
+
+public class Issue4167IT extends ITBase {
+
+    @Test
+    public void test() throws Exception {
+        HtmlPage page = webClient.getPage(webUrl + "issue4167.xhtml");
+
+        assertTrue(page.getHtmlElementById("result1").asNormalizedText().isEmpty());
+        assertTrue(page.getHtmlElementById("result2").asNormalizedText().isEmpty());
+        assertTrue(page.getHtmlElementById("result3").asNormalizedText().isEmpty());
+
+        HtmlSubmitInput button = (HtmlSubmitInput) page.getHtmlElementById("form:button");
+        page = button.click();
+
+        assertTrue(page.getHtmlElementById("result1").asNormalizedText().equals("issue4167"));
+        assertTrue(page.getHtmlElementById("result2").asNormalizedText().equals("issue4167"));
+        assertTrue(page.getHtmlElementById("result3").asNormalizedText().equals("issue4167"));
+    }
+
+}
