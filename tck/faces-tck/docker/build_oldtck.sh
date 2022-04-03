4c421573423af46132d63ddf87aea5fcb153c76e
Alwin Joseph <44426046+alwin-joseph@users.noreply.github.com>
2022-04-04 02:58:37 +0530

    fix old tck build and run scripts

--- tck/faces-tck/docker/build_oldtck.sh
+++ tck/faces-tck/docker/build_oldtck.sh
@@ -14,11 +14,13 @@
 #
 # SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
 
+export WORKSPACE="$1"
+
 if [ -z "$WORKSPACE" ]; then
   export WORKSPACE=`pwd`
 fi
 
-export BASEDIR=${WORKSPACE}/faces-tck
+export BASEDIR=${WORKSPACE}
 
 if [ -z "$JAKARTA_JARS" ]; then
   export JAKARTA_JARS=$BASEDIR/lib
