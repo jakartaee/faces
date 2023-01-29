fb74d801277004db12ceb9d4d6971b963a9406e9
Piotrek Zygielo <piotr.zygielo@gmail.com>
2023-01-29 21:42:15 +0100

    Remove temporary files on exit

--- tck/faces40/inputFile/src/test/java/ee/jakarta/tck/faces/test/servlet50/inputfile/Spec1555IT.java
+++ tck/faces40/inputFile/src/test/java/ee/jakarta/tck/faces/test/servlet50/inputfile/Spec1555IT.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Contributors to the Eclipse Foundation.
+ * Copyright (c) 2021, 2023 Contributors to the Eclipse Foundation.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -164,6 +164,7 @@ public class Spec1555IT {
 
     private static File generateTempFile(String name, String ext, int size) throws IOException {
         Path path = Files.createTempFile(name, "." + ext);
+        path.toFile().deleteOnExit();
         byte[] content = new byte[size];
         Files.write(path, content, APPEND);
         return path.toFile();
