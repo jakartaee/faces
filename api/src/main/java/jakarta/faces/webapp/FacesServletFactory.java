318003e03141a23d57cffe610fb95f11fa613511
Automated workflow <m_riem@hotmail.com>
2023-11-26 10:53:38 -0600

    Fixes #1861 - Introduce FacesServletFactory

--- api/src/main/java/jakarta/faces/webapp/FacesServletFactory.java
+++ api/src/main/java/jakarta/faces/webapp/FacesServletFactory.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 1997, 2020 Oracle and/or its affiliates. All rights reserved.
- * Copyright (c) 2021 Contributors to Eclipse Foundation.
+ * Copyright (c) 2023 Contributors to Eclipse Foundation.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -16,6 +16,7 @@
  */
 package jakarta.faces.webapp;
 
+import jakarta.faces.FacesWrapper;
 import jakarta.servlet.Servlet;
 import jakarta.servlet.ServletConfig;
 
@@ -24,11 +25,12 @@ import jakarta.servlet.ServletConfig;
  * 
  * @author Manfred Riem
  */
-public abstract class FacesServletFactory {
+public abstract class FacesServletFactory implements FacesWrapper<FacesServletFactory> {
     
     /**
      * Get the Faces Servlet.
      * 
+     * @param config the servlet configuration.
      * @return the Faces Servlet.
      */
     public abstract Servlet getFacesServlet(ServletConfig config);
