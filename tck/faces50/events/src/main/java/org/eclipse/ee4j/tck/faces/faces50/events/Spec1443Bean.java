b80cbfc6d0d9af830be951fca78d79cc304c8ca1
Bauke Scholtz <balusc@gmail.com>
2025-02-02 11:56:11 -0400

    renamed phaseEvents to events

--- tck/faces50/events/src/main/java/org/eclipse/ee4j/tck/faces/faces50/events/Spec1443Bean.java
+++ tck/faces50/events/src/main/java/org/eclipse/ee4j/tck/faces/faces50/events/Spec1443Bean.java
@@ -13,7 +13,7 @@
  *
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  */
-package org.eclipse.ee4j.tck.faces.faces50.phaseEvents;
+package org.eclipse.ee4j.tck.faces.faces50.events;
 
 import java.util.ArrayList;
 import java.util.List;
