1b8b52df5921977f49bda19a185f8e83d3b222fd
Bauke Scholtz <balusc@gmail.com>
2025-03-01 15:52:50 -0400

    TCK for #1940

---
+++ tck/faces50/uiinput/src/main/java/ee/jakarta/tck/faces/test/faces50/uiinput/Spec1940Bean.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) Contributors to the Eclipse Foundation.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * https://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+ * which is available at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the Eclipse
+ * Public License v. 2.0 are satisfied: GPL-2.0 with Classpath-exception-2.0 which
+ * is available at https://openjdk.java.net/legal/gplv2+ce.html.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0 or Apache-2.0
+ */
+
+package ee.jakarta.tck.faces.test.faces50.uiinput;
+
+import java.io.Serializable;
+
+import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Named;
+
+@Named
+@ViewScoped
+public class Spec1940Bean implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private String value;
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
