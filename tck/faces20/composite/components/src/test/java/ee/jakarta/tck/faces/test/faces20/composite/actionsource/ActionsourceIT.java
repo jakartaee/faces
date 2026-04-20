f488db3fce12c37e9e80ce8e25fd3871305bb2dd
Bauke Scholtz <balusc@gmail.com>
2026-04-20 17:31:35 -0400

    consolidate composite/components to minimize amount of WARs; saved almost 2 minutes

---
+++ tck/faces20/composite/components/src/test/java/ee/jakarta/tck/faces/test/faces20/composite/actionsource/ActionsourceIT.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2026 Contributors to the Eclipse Foundation.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0, which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the
+ * Eclipse Public License v. 2.0 are satisfied: GNU General Public License,
+ * version 2 with the GNU Classpath Exception, which is available at
+ * https://www.gnu.org/software/classpath/license.html.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ */
+package ee.jakarta.tck.faces.test.faces20.composite.actionsource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import ee.jakarta.tck.faces.test.util.selenium.BaseITNG;
+import ee.jakarta.tck.faces.test.util.selenium.WebPage;
+
+class ActionsourceIT extends BaseITNG {
+
+    @Test
+    void compositeActionSourceTagTest() {
+        // case 1: both 'target' and 'name' attributes specified
+        WebPage pageOne = getPage("faces/actionsource/targetandname.xhtml");
+        testActionSource(pageOne, "button");
+
+        // case 2: only 'name' attribute specified
+        WebPage pageTwo = getPage("faces/actionsource/notargets.xhtml");
+        testActionSource(pageTwo, "testOne");
+    }
+
+    private void testActionSource(WebPage page, String buttonId) {
+        WebElement button = findByIdSuffix(page, buttonId);
+        page.guardHttp(button::click);
+
+        WebElement span = findByIdSuffix(page, "result");
+        assertEquals("PASSED", span.getText(), "Span value");
+    }
+
+    private static WebElement findByIdSuffix(WebPage page, String id) {
+        String suffix = ":" + id;
+        return page.findElement(By.xpath(
+            "//*[@id='" + id + "'"
+            + " or substring(@id, string-length(@id) - " + (suffix.length() - 1) + ") = '" + suffix + "']"));
+    }
+}
